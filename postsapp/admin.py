2e0de27d1bf3bfdc830ede3b31af032acd0e22f6
ThisIsTheBiggestDevGuy <goldfighter368@gmail.com>
2024-01-15 20:17:05 +0330

    chating system all added.

--- postsapp/admin.py
+++ postsapp/admin.py
@@ -1,9 +1,10 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import Category, Attribute, Post, AttributeValue
+from .models import Category, Attribute, Post, AttributeValue, Chat
 
 admin.site.register(Category)
 admin.site.register(AttributeValue)
 admin.site.register(Attribute)
 admin.site.register(Post)
+admin.site.register(Chat)
