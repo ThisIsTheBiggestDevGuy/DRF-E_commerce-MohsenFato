3afcef3f114a950395cb40126043758620b83e1a
ThisIsTheBiggestDevGuy <goldfighter368@gmail.com>
2024-01-16 20:15:30 +0330

    usrls for SellerProfileDetailView and SellerReviewCreateView have been added

--- usersapp/urls.py
+++ usersapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import (LoginView, RegisterUserView)
+from .views import (LoginView, RegisterUserView,
+                    SellerProfileDetailView, SellerReviewCreateView)
 
 urlpatterns = [
     path('api-token-auth/', LoginView.as_view(), name='api_toke_auth'),
     path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('seller-profile/<int:pk>/', SellerProfileDetailView.as_view(), name='seller-profile-detail'),
+    path('seller-reviews/',SellerReviewCreateView.as_view(),name='seller-review-create'),
 ]
