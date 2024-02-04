a9792fbcfcbf56c632099674a7af5246c6a55339
ThisIsTheBiggestDevGuy <goldfighter368@gmail.com>
2024-02-04 12:49:58 +0330

    PostListCreateView modified to handle the category id and provide it for the access to its related attributes when creating a post

--- postsapp/views.py
+++ postsapp/views.py
@@ -2,6 +2,7 @@
 from .models import Post, Category, Attribute, Chat
 from postsapp.serializers import PostSerializer, CategorySerializer, ChatSerializer
 # Create your views here.
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from django.core.exceptions import PermissionDenied
 from rest_framework import generics, status
@@ -21,46 +22,22 @@ class PostListCreateView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def perform_create(self, serializer):
-        # Set the user field during post creation
-        if self.request.user.is_authenticated:
-            serializer.save(user=self.request.user)
-            # check if user wants to hide phone number
-            hide_phone_number = self.request.data.get('hide_phone_number', False)
-            if hide_phone_number:
-                serializer.validated_data['phone_number'] = None
-            serializer.save(user=self.request.user)
-        else:
-            raise PermissionDenied("Not allowed")
-
     def get_queryset(self):
         queryset = Post.objects.all()
 
-        # searching functionality:
+        # Searching functionality:
         search_query = self.request.query_params.get('search', None)
         if search_query:
             queryset = queryset.filter(
                 Q(title__icontains=search_query) |
                 Q(description__icontains=search_query)
             )
-        # filtering based on categories or other criteria
+        # Filtering based on categories or other criteria
         category_id = self.request.query_params.get('category_id', None)
 
-        def get_category_attributes(category_id):
-            try:
-                category = Category.objects.get(pk=category_id)
-                attributes = Attribute.objects.filter(category=category)
-                return [attribute.name for attribute in attributes]
-            except Category.DoesNotExist:
-                # The case where the category doesn't exist
-                return Response({'error': 'Category not found'}, status=status.HTTP_404_NOT_FOUND)
-            except Attribute.DoesNotExist:
-                # The case where there are no attributes for the category
-                return Response({'message': 'No attributes found for the category'}, status=status.HTTP_200_OK)
-        category_attributes = get_category_attributes(category_id)
-
         if category_id:
-            category_attributes = get_category_attributes(category_id)
+            # Use the get_category_attributes method to fetch attributes
+            category_attributes = self.get_category_attributes(category_id)
             # Loop through the category attributes and apply filters
             for attribute in category_attributes:
                 attribute_name = attribute.name
@@ -70,6 +47,30 @@ class PostListCreateView(generics.ListCreateAPIView):
                                                attribute_values__value=attribute_value)
 
         return queryset
+
+    def get_category_attributes(self, category_id):
+        category = get_object_or_404(Category, pk=category_id)
+        attributes = Attribute.objects.filter(category=category)
+        return attributes
+
+    def perform_create(self, serializer):
+        # Set the user field during post creation
+        if self.request.user.is_authenticated:
+            serializer.save(user=self.request.user)
+            # Check if the user wants to hide the phone number
+            hide_phone_number = self.request.data.get('hide_phone_number', False)
+            if hide_phone_number:
+                serializer.validated_data['phone_number'] = None
+            serializer.save(user=self.request.user)
+
+            # Fetch and include category attributes in the response
+            category_id = self.request.data.get('category', None)
+            if category_id:
+                category_attributes = self.get_category_attributes(category_id)
+                serializer.context['category_attributes'] = category_attributes
+
+        else:
+            raise PermissionDenied("Not allowed")
 
 
 class PostRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
@@ -92,4 +93,3 @@ class ChatListCreateView(generics.ListCreateAPIView):
         serializer = self.get_serializer(queryset, many=True)
         chats_serializer = ChatSerializer(Chat.objects.all(), many=True)
         return Response({'posts': serializer.data, 'chats': chats_serializer.data})
-
